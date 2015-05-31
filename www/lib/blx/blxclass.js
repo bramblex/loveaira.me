f66d5f3ca82efb080d64dd4dd1d581cc9a0081c6
brambles <qjnight@gmail.com>
2015-06-01 05:07:50 +0800

    完成代码

--- www/lib/blx/blxclass.js
+++ www/lib/blx/blxclass.js
@@ -1,17 +1,27 @@
-
 ;(function(){
-  'user script';
-  var BlxClass = function(){
+
+  var BlxClass = function(){};
+
+  BlxClass.prototype.extend = function extend(name, parent, constructor){
+
+    eval('var child = function '+ name +'(){\
+         if (this === window){ \
+         return new arguments.callee();\
+         }\
+         this.parent = this.prototype;\
+         constructor.apply(this, arguments);}');
+
+         child.prototype = new parent();
+         return child;
   };
 
-  BlxClass.prototype.extend = function extend(parent, constructor){
-    var child = function(){
-      this.parent = this.prototype
-      constructor.apply(this, constructor);
-    };
-    child.prototype = new parent();
-    return child;
+  BlxClass.alias = function alias(name){
+    eval('var aliasClosure = function '+name+'(){\
+         return this[name].apply(this, arguments);\
+         };');
+    return aliasClosure;
   };
 
-  return new BlxClass();
-});
+  window.BlxClass = new BlxClass();
+
+})();
