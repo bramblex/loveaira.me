f964e637f0e429776119b4cf71306d2b6cd2086a
brambles <qjnight@gmail.com>
2015-07-21 17:44:43 +0800

    add system controller

--- controllers/system.js
+++ controllers/system.js
@@ -2,10 +2,15 @@
 var process = require('child_process');
 
 module.exports = Controller.extend('system')
+
 .method('update', function(){
   var _this = this;
   process.exec('git pull origin master',function(err, stdout, stderr){
     _this.response.end(stdout + stderr);
   });
 })
-;
+
+.method('reload', function(){
+  this.response.end('system reload!');
+  throw 'system reload!';
+});
