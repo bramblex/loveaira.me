7b7d79d64fd37008bd6ddf88733f6a16918c555a
brambles <qjnight@gmail.com>
2016-02-21 09:18:21 +0800

    TnT

--- server/src/Model/User.purs
+++ server/src/Model/User.purs
@@ -21,13 +21,9 @@ instance isValuePassword :: IsValue Password where
 table_name = "user"
 schema = [ "username" .=> "VARCHAR(255) UNIQUE NOT NULL"
          , "password" .=> "VARCHAR(255) NOT NULL"
-         , "role" .=> "VARCHAR(255) DEFAULT \"auth\""]
+         , "role" .=> "VARCHAR(255) DEFAULT \"auth\"" ]
 init = do
   createTable table_name schema
-  -- insertOrUpdateUser [ "id" .= 0
-  --                    , "username" .= "admin"
-  --                    , "role" .= "admin"
-  --                    , "password" .= Password "admin" ]
 
 findUser = find table_name
 firstUser = first table_name
