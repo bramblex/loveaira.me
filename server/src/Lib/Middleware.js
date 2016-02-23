8e184964b5c3bcb152da20d65401d88f46f87be4
brambles <qjnight@gmail.com>
2016-02-23 23:53:52 +0800

    fix bug

--- server/src/Lib/Middleware.js
+++ server/src/Lib/Middleware.js
@@ -7,5 +7,6 @@ exports['static'] = require('express')['static'];
 exports.setCookiesMaxAge = function(maxAge){
     return function(req, res, next){
         req.sessionOptions.maxAge = maxAge * 1000;
+        next();
     };
 };
